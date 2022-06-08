41406f32ce4b33e20dff3afa25e92ac106563032
Jehison Galarza <jehisonsc045@gmail.com>
2022-06-08 18:28:34 -0500

    Don Raul's Hardware Store --- Project creation

---
+++ src/main/java/com/donrauls/hadwarestore/HadwarestoreApplication.java
@@ -0,0 +1,13 @@
+package com.donrauls.hadwarestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HadwarestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HadwarestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donrauls/hadwarestore/HadwarestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donrauls.hadwarestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadwarestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
